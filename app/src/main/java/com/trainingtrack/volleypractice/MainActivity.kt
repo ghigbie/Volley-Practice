5f34890bf164a2838eb67ac9a123391adb19cc06
ghigbie <gehigbie@gmail.com>
2020-03-15 17:44:46 -0700

    Initial commit

---
+++ app/src/main/java/com/trainingtrack/volleypractice/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.trainingtrack.volleypractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
