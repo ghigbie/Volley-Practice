00e1533f86c3febf9382f47f24d0939125457829
ghigbie <gehigbie@gmail.com>
2020-03-15 18:48:29 -0700

    Add new method

--- app/src/main/java/com/trainingtrack/volleypractice/MainActivity.kt
+++ app/src/main/java/com/trainingtrack/volleypractice/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.trainingtrack.volleypractice
 
+import android.app.DownloadManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
+
+    val que = Volley.newRequestQueue(this)
+    val url = "https://jsonplaceholder.typicode.com/todos/1"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        val jsonObjectRequest: JsonObjectRequest = JsonObjectRequest(
+                Request.Method.GET, url, null,
+                Response.Listener { response ->
+
+                }
+        )
+
+
     }
+
+
+
 }
